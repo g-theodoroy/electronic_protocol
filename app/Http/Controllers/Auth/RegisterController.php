009a585a53be3a4f95998fa354fcbf70d1e92d9a
g-theodoroy <g.theodoroy@gmail.com>
2017-06-07 12:28:56 +0300

    edited redirection after register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/home/list';
 
     /**
      * Create a new controller instance.
