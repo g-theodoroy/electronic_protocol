bd6b52decabf37b99ca33ec32f84ebae3d3dab06
Γεώργιος Θεοδώρου <g.theodoroy@gmail.com>
2017-06-08 23:41:54 +0300

    Create ProtocolController.php

--- app/Http/Controllers/ProtocolController.php
+++ app/Http/Controllers/ProtocolController.php
@@ -138,6 +138,7 @@ use DB;
                 $url = 'https://api.github.com/repos/g-theodoroy/electronic_protocol/commits';
                 $curl = curl_init();
                 curl_setopt($curl, CURLOPT_CONNECTTIMEOUT ,1); 
+                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
                 curl_setopt($curl, CURLOPT_URL, $url);
                 curl_setopt($curl,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13'); // Set a user agent
                 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
