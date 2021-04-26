caaa5874182979fe640a608915a09e39cb9bfa9e
george <g.theodoroy@gmail.com>
2021-04-26 17:06:20 +0300

    Βελτίωση λίστας κατά την πληκτρολόγηση  Αλλαγές προς υποβολή: 	τροποποίηση:     app/Http/Controllers/ProtocolController.php

--- app/Http/Controllers/ProtocolController.php
+++ app/Http/Controllers/ProtocolController.php
@@ -2208,17 +2208,16 @@ class ProtocolController extends Controller
     // βρίσκει τις καταχωρίσεις που ταιριάζουν με αυτά που πληκτρολογεί ο χρήστης και επιστρέφει λίστα
     public function getValues($term, $field, $id, $divId, $multi)
     {
-        $protocols = Protocol::latest()->take(1000);
-        $protocols = $protocols->where($field, 'like', "%" . $term . "%")->groupBy($field)->orderBy(DB::raw("count($field)"), 'DESC')->get($field)->take(10);
-        $sortedProtocols = $protocols->sortBy($field);
+        $protocols = Protocol::where($field, 'like', "%" . $term . "%")->groupBy($field)->orderBy(DB::raw("count($field)"), 'DESC')->get($field)->take(10);
+        $sortedProtocols = $protocols->sortBy($field)->values()->all();
         if (!$protocols) {
             return;
         }
         $output = '';
         if ($multi) {
             $valuesArray = [];
-            foreach ($sortedProtocols->values()->all() as $protocol) {
-                $valuesArray = array_merge($valuesArray, preg_split('/\s*,\s*/', $protocol->$field));
+            foreach ($sortedProtocols as $protocol) {
+                $valuesArray = array_merge($valuesArray, preg_split('/\s*,\s*/', $protocol->$field ));
             }
             $collection = collect($valuesArray)->unique()->sortBy('Key')->values()->all();
             foreach ($collection as $value) {
@@ -2228,7 +2227,7 @@ class ProtocolController extends Controller
                 }
             }
         } else {
-            foreach ($sortedProtocols->values()->all() as $protocol) {
+            foreach ($sortedProtocols as $protocol) {
                 $value = $protocol->$field;
                 $output .= '<li style="cursor: pointer"><a onclick="javascript:appendValue(\'' . $id . '\',\'' . $value . '\',\'' . $divId . '\',\'' . $multi . '\')">' . e($value) . '</a></li>
             ';
